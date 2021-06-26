450c2c38264a0ce1bbdf423dc7632c12ddcf70a9
dezhizhang <1541609448@qq.com>
2021-06-26 09:24:53 +0800

    添加文件

--- web/config/config.default.js
+++ web/config/config.default.js
@@ -1,3 +1,4 @@
+
 /* eslint valid-jsdoc: "off" */
 
 'use strict';
@@ -17,6 +18,17 @@ module.exports = appInfo => {
 
   // add your middleware config here
   config.middleware = [];
+
+  config.mongo = {
+    client: {
+      host:"127.0.0.1",
+      port:"27017",
+      name:"share",
+      user:"",
+      password:"",
+      options:{},
+    }
+  }
 
   // add your user config here
   const userConfig = {
--- web/config/plugin.js
+++ web/config/plugin.js
@@ -1,9 +1,23 @@
+/*
+ * :file description: 
+ * :name: /share/web/config/plugin.js
+ * :author: 张德志
+ * :copyright: (c) 2021, Tungee
+ * :date created: 2021-06-26 09:12:26
+ * :last editor: 张德志
+ * :date last edited: 2021-06-26 09:20:38
+ */
 'use strict';
 
 /** @type Egg.EggPlugin */
-module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
-};
+// module.exports = {
+//   // had enabled by egg
+//   // static: {
+//   //   enable: true,
+//   // }
+// };
+
+exports.mongoose = {
+  enable:true,
+  package:'egg-mongoose'
+}
