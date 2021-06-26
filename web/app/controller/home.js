43815cbb15a82c8d0c3636f00b75db134de3f2da
dezhizhang <1541609448@qq.com>
2021-06-26 10:10:10 +0800

    update

--- web/app/controller/home.js
+++ web/app/controller/home.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2021, Tungee
  * :date created: 2021-06-26 09:12:26
  * :last editor: 张德志
- * :date last edited: 2021-06-26 09:27:35
+ * :date last edited: 2021-06-26 09:34:04
  */
 'use strict';
 
@@ -13,7 +13,7 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.render('/index.html');
+    await this.ctx.render('/index.html');
   }
 }
 
