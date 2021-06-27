94d3764457869e6d4b77c61137cc12a96fba3d50
dezhizhang <1541609448@qq.com>
2021-06-27 12:23:15 +0800

    feat:表表组件

--- app/lib/pages/ListPage.dart
+++ app/lib/pages/ListPage.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -22,17 +24,34 @@ class ListContent extends StatefulWidget {
 class _ListContent extends State<ListContent> {
   List<Map> list = [];
   var page = 1;
+  ScrollController _scrollController = new ScrollController();
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     this.loadData();
+    //监听滚动条事件
+    _scrollController.addListener(() {
+      print(_scrollController.position.pixels); //获取滚动条下拉的距离
+
+      print(_scrollController.position.maxScrollExtent); //获取整个页面的高度
+
+      if (_scrollController.position.pixels >
+          _scrollController.position.maxScrollExtent - 40) {
+        this.loadData();
+      }
+    });
   }
 
   loadData() async {
     const url = 'http://127.0.0.1:7001/api/list/search?page=1';
     Response response = await Dio().get(url);
-    print(response);
+    var data = json.decode(response.data.toString());
+    if (data['code'] == 200) {
+      setState(() {
+        list = data['data'];
+      });
+    }
   }
 
   Future<void> handleRefresh() async {
@@ -44,11 +63,14 @@ class _ListContent extends State<ListContent> {
     // TODO: implement build
     return Container(
       child: RefreshIndicator(
-        onRefresh: handleRefresh,
-        child: ListView(
-          children: <Widget>[Text('hello')],
-        ),
-      ),
+          onRefresh: handleRefresh,
+          child: ListView.builder(
+              itemCount: list.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  child: Image.network('http://127.0.0.1:7001${list[index]}'),
+                );
+              })),
     );
   }
 }
