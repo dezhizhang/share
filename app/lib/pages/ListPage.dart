fd13cdecb12bdd3968ee6e1d8887ac3c934413ad
dezhizhang <1541609448@qq.com>
2021-06-27 11:49:27 +0800

    update

--- app/lib/pages/ListPage.dart
+++ app/lib/pages/ListPage.dart
@@ -1,12 +1,14 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class ListPage extends StatelessWidget {
+  Future<void> onRefresh() async {}
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text('列表页'),
+        title: Text('上拉刷新下拉加载更多'),
       ),
       body: ListContent(),
     );
@@ -18,9 +20,35 @@ class ListContent extends StatefulWidget {
 }
 
 class _ListContent extends State<ListContent> {
+  List<Map> list = [];
+  var page = 1;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this.loadData();
+  }
+
+  loadData() async {
+    const url = 'http://127.0.0.1:7001/api/list/search?page=1';
+    Response response = await Dio().get(url);
+    print(response);
+  }
+
+  Future<void> handleRefresh() async {
+    this.loadData();
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container();
+    return Container(
+      child: RefreshIndicator(
+        onRefresh: handleRefresh,
+        child: ListView(
+          children: <Widget>[Text('hello')],
+        ),
+      ),
+    );
   }
 }
