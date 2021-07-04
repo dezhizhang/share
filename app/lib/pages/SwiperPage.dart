2ab39ed8f1b1631ce2a91a5bd27d05822c8116ef
dezhizhang <1541609448@qq.com>
2021-07-05 07:37:08 +0800

    swiper

--- app/lib/pages/SwiperPage.dart
+++ app/lib/pages/SwiperPage.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class SwiperPage extends StatelessWidget {
   @override
@@ -21,9 +21,73 @@ class SwiperContent extends StatefulWidget {
 }
 
 class _SwiperContent extends State<SwiperContent> {
+  List list = [];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this.loadData();
+  }
+
+  loadData() async {
+    const url = 'http://www.xiaozhi.shop/api/list/search?page=1';
+    Response response = await Dio().get(url);
+    var data = json.decode(response.toString());
+    if (data['code'] == 200) {
+      var arr = [];
+      for (var i = 0; i < 10000; i++) {
+        arr.add(data['data'][1]);
+      }
+      setState(() {
+        list = arr;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Text('hello');
+    return Banner(list: this.list);
+  }
+}
+
+class Banner extends StatelessWidget {
+  final List list;
+  final double? height;
+  final EdgeInsetsGeometry? padding;
+  Banner({Key? key, required this.list, this.height = 160, this.padding})
+      : super(key: key);
+  _banner() {
+    var right = 10 + (padding?.horizontal ?? 0) / 2;
+    return Swiper(
+      autoplay: true,
+      itemCount: list.length,
+      pagination: SwiperPagination(),
+      control: SwiperControl(),
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(child: _image(list[index]), onTap: () {});
+      },
+    );
+  }
+
+  _image(item) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(6)),
+          child: Image.network(item['url']),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      height: height,
+      child: _banner(),
+    );
   }
 }
